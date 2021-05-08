ab0a55cb38978a7f70d4598c609c90d5a7675786
tom1k <tomascosta@ua.pt>
2021-05-08 12:27:25 +0100

    Added PSource for UC1

--- code/PA2_G6/src/UC_1/PSOURCE.java
+++ code/PA2_G6/src/UC_1/PSOURCE.java
@@ -1,13 +1,34 @@
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author tomascosta
  */
 public class PSOURCE {
-    
+    // Here, declare filepath, java socket
+    private static final String filePath = System.getProperty("user.dir") + "/src/Data/sensors.txt";
+
+    public static void main(String args[]) {
+        try {
+        //the file to be opened for reading  
+            FileInputStream fis = new FileInputStream(filePath);
+            Scanner sc = new Scanner(fis);   
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+                System.out.println("Sending to Producer"); // Here send on the server conn to producer, usar server do trab1?
+            }
+            sc.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
